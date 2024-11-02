1534225b1203d73940cd16dd9ebe0271a16e3897
Ayushmaurya <ayushmaurya3596@gmail.com>
2024-11-03 02:27:17 +0530

    Updated frontend with new dependencies and fixed build issues. Updated backend configuration.

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 // MongoDB Atlas connection string
 
+console.log('Mongo URI:', process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
