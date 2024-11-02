604ee53a6c14f2346fc32f0f8e008ff0810f4ea1
Ayushmaurya <ayushmaurya3596@gmail.com>
2024-11-03 02:18:45 +0530

    Updated frontend with new dependencies and fixed build issues. Updated backend configuration.

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ app.use(express.json()); // To parse incoming JSON requests
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 // MongoDB Atlas connection string
-const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
